7d8bf583c031c4ce3bcc25f9cb24b48f221d1934
srghma <srghma@gmail.com>
2018-08-24 13:21:43 +0300

    fix: demo works

--- src/DateRangeFilter.tsx
+++ src/DateRangeFilter.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as PropTypes from "prop-types";
 import * as moment from "moment";
 
 import {
@@ -73,18 +74,18 @@ export class DateRangeFilter extends SearchkitComponent<DateRangeFilterProps, an
   accessor:DateRangeAccessor
 
   static propTypes = defaults({
-    fromDate:React.PropTypes.object,
-    toDate:React.PropTypes.object,
-    fromDateField:React.PropTypes.string.isRequired,
-    toDateField:React.PropTypes.string.isRequired,
-    title:React.PropTypes.string.isRequired,
-    id:React.PropTypes.string.isRequired,
+    fromDate:PropTypes.object,
+    toDate:PropTypes.object,
+    fromDateField:PropTypes.string.isRequired,
+    toDateField:PropTypes.string.isRequired,
+    title:PropTypes.string.isRequired,
+    id:PropTypes.string.isRequired,
     containerComponent:RenderComponentPropType,
     calendarComponent:RenderComponentPropType,
-    rangeFormatter:React.PropTypes.func,
-    fieldOptions:React.PropTypes.shape({
-      type:React.PropTypes.oneOf(["embedded", "nested", "children"]).isRequired,
-      options:React.PropTypes.object
+    rangeFormatter:PropTypes.func,
+    fieldOptions:PropTypes.shape({
+      type:PropTypes.oneOf(["embedded", "nested", "children"]).isRequired,
+      options:PropTypes.object
     }),
   }, SearchkitComponent.propTypes)
 
